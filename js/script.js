65305cce4716d00d572583a8492a187d0d67b211
Bruno Feitosa <brunofeitosa.rj25@gmail.com>
2023-06-24 14:28:12 -0300

    Adiciona Animações e Cria JavaScript :art:

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const mario = document.querySelector('.mario');
+
+/* const jump = () => {
+    mario.classList.add('jump');
+}
+*/
+
+function jump (){
+    mario.classList.add('jump');
+}
+
+document.addEventListener('keydown', jump);

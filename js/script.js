96d2fbe1128b76e67efcb44882d11a885bb43b41
Bruno Feitosa <brunofeitosa.rj25@gmail.com>
2023-06-25 03:28:50 -0300

    Finaliza Versão 1.0 do The Mario Game :gift:

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 //Puxar Elementos
 const mario = document.querySelector('.mario')
 const pipe = document.querySelector('.pipe')
+const clouds = document.querySelector('.clouds')
 
 document.addEventListener('keydown', jump)
 
@@ -16,19 +17,31 @@ function jump() {
     }, 500)
 }
 
-
+// Função do Pulo do Mario
 function loop() {
     setInterval(() => {
+
         const pipePosition = pipe.offsetLeft
+        const cloudsPosition = clouds.offsetLeft
         const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '')
 
         if (pipePosition <= 85 && pipePosition > 0 && marioPosition < 75) {
 
+            // Código para Parar as Animações
             pipe.style.animation = 'none'
             pipe.style.left = `${pipePosition}px`
 
             mario.style.animation = 'none'
             mario.style.bottom = `${marioPosition}px`
+
+            clouds.style.animation = 'none'
+            clouds.style.left = `${cloudsPosition}px`
+
+            mario.src = './images/game-over.png'
+            mario.style.width = '50px'
+            mario.style.marginLeft = '30px'
+
+            clearInterval(loop)
         }
 
     }, 10)

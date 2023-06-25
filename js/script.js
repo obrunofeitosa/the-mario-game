aac8c78b58711e77705f79142c21fd01618b7f0c
Bruno Feitosa <brunofeitosa.rj25@gmail.com>
2023-06-25 02:41:11 -0300

    Adiciona Functions no JavaScript :art:
    Jump, Loop, Keydown

--- js/script.js
+++ js/script.js
@@ -1,12 +1,38 @@
-const mario = document.querySelector('.mario');
+//Puxar Elementos
+const mario = document.querySelector('.mario')
+const pipe = document.querySelector('.pipe')
 
-/* const jump = () => {
+document.addEventListener('keydown', jump)
+
+// Chama a Função Loop 
+loop();
+
+// Função do Pulo do Mario
+function jump() {
     mario.classList.add('jump');
-}
-*/
 
-function jump (){
-    mario.classList.add('jump');
+    setTimeout(() => {
+        mario.classList.remove('jump')
+    }, 500)
 }
 
-document.addEventListener('keydown', jump);
+
+function loop() {
+    setInterval(() => {
+        const pipePosition = pipe.offsetLeft
+        const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '')
+
+        if (pipePosition <= 85 && pipePosition > 0 && marioPosition < 75) {
+
+            pipe.style.animation = 'none'
+            pipe.style.left = `${pipePosition}px`
+
+            mario.style.animation = 'none'
+            mario.style.bottom = `${marioPosition}px`
+        }
+
+    }, 10)
+}
+
+// Chama a Função Loop 
+loop();
